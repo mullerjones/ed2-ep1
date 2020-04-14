8f7101968a653e5d856aed91a783ea5f155a5ddd
mullerjones <ale.mullerjones@gmail.com>
2020-04-14 19:38:10 -0300

    Adicionada interface e nomes ajustados

---
+++ ep1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+    std::string arq;
+    std::string mode;
+    
+    arq  = argv[1];
+    mode = argv[2];
+    
+
+    return 0;
+}
